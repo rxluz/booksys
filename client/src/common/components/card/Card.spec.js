749b7be96592011fe5b1200db7fce2561638c492
rxluz <hi@rxluz.dev>
2021-10-03 18:16:18 +0100

    feat(stateless-interfaces): introduce basic card

--- client/src/common/components/card/Card.spec.js
+++ client/src/common/components/card/Card.spec.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import Chip from '../Chip'
+import Input from '../Input'
 
-test('renders hello world inside the chip', () => {
-  const { getByText } = render(<Chip>Hello world</Chip>)
+test('Renders input element properly', () => {
+  const { getByText } = render(<Input />)
   const linkElement = getByText(/Hello/i)
   expect(linkElement).toBeInTheDocument()
 })
