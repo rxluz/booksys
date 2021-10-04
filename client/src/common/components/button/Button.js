992b2b38fb1002d1a6decdb32f5144823f387ad8
rxluz <hi@rxluz.dev>
2021-10-04 18:19:26 +0100

    feat(stateless-interfaces): introduce button component

--- client/src/common/components/button/Button.js
+++ client/src/common/components/button/Button.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 import './Button.scss'
 
-const Button = ({ children, isDisabled }) => <button disabled={isDisabled}>{children}</button>
+const Button = ({ children, isDisabled, onClick }) => (
+  <button className="button" onClick={onClick} disabled={isDisabled}>
+    {children}
+  </button>
+)
 
 export default Button
