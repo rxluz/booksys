eec8f324d7c5a48d6d9155471b216feeda17d437
rxluz <hi@rxluz.dev>
2021-10-05 20:58:13 +0100

    feat(stateless-interfaces): introduce pagination component and office card

---
+++ client/src/common/components/officeCard/OfficeCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import moment from 'moment'
+import * as generalConstants from 'common/utils/general.constants'
+import { BiChair } from 'react-icons/bi'
+import Button from 'common/components/button'
+
+import './OfficeCard.scss'
+
+const OfficeCard = ({ time, seat, onBook, translate }) => (
+  <div className="office-card">
+    <div className="office-card__content">
+      <div className="office-card__time">
+        {moment.unix(time).format(generalConstants.MOMENT_TIME)}
+      </div>
+      <div className="office-card__seats">
+        <div className="office-card__seats--icon">
+          <BiChair />
+        </div>
+        <div className="office-card__seats--text">{seat}</div>
+      </div>
+    </div>
+    <div className="office-card__footer">
+      <Button onClick={() => onBook({ seat, time })}>{translate('Book')}</Button>
+    </div>
+  </div>
+)
+
+export default OfficeCard
---
+++ client/src/common/components/officeCard/index.js
@@ -0,0 +1,4 @@
+import OfficeCard from './OfficeCard'
+
+export * from './OfficeCard'
+export default OfficeCard
