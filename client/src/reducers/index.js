1d48c42cafc2392625a5575b659dce7f85b17291
rxluz <hi@rxluz.dev>
2021-10-05 21:01:07 +0100

    feat(stateless-interfaces): introduce booking store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 
 import settings from './settings'
 import company from './company'
+import booking from './booking'
 
 export default combineReducers({
   settings,
   company,
+  booking,
   i18nState,
 })
