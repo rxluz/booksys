5c61e0552a005c39bc622b73a0aee624c68eeb8b
rxluz <hi@rxluz.dev>
2021-10-07 12:04:38 +0100

    feat(storybook): introduce pagination story

---
+++ client/src/common/components/pagination/Pagination.stories.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Pagination from './Pagination'
+import { action } from '@storybook/addon-actions'
+import { BADGE } from '@geometricpanda/storybook-addon-badges'
+import { addDefaults, addArgTypes } from 'common/utils/storybookMode'
+
+const defaultProps = { onPageChange: action('onClickCallToAction') }
+
+const addTheme = addDefaults({
+  defaultProps,
+  figmaURL: 'https://www.figma.com/file/pFx6z24OMgktLQL64aKYjG/Booksys?node-id=168%3A79',
+})
+
+export default {
+  title: 'Pagination',
+  parameters: {
+    badges: [BADGE.STABLE],
+  },
+  argTypes: addArgTypes({
+    current: {
+      defaultValue: 1,
+      type: 'number',
+    },
+    total: {
+      defaultValue: 10,
+      type: 'number',
+    },
+  }),
+}
+
+export const PaginationOptions = addTheme((props) => <Pagination {...props} />)
