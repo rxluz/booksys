4d2c0e043950c115afbcdc96339838214e6eea41
rxluz <hi@rxluz.dev>
2021-10-07 12:03:23 +0100

    feat(storybook): introduce button story

--- client/src/common/components/button/Button.stories.js
+++ client/src/common/components/button/Button.stories.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import Button from './Button'
+import { action } from '@storybook/addon-actions'
+import { BADGE } from '@geometricpanda/storybook-addon-badges'
+import { addDefaults, addArgTypes } from 'common/utils/storybookMode'
 
-import { Light, Dark } from 'common/utils/storybookMode'
+const defaultProps = { onClick: action('onClick') }
 
-export default { title: 'Button' }
+const addTheme = addDefaults({
+  defaultProps,
+  figmaURL: 'https://www.figma.com/file/pFx6z24OMgktLQL64aKYjG/Booksys?node-id=147%3A2',
+})
 
-export const buttonLight = () => (
-  <Light>
-    <Button />
-  </Light>
-)
+export default {
+  title: 'Button',
+  parameters: {
+    badges: [BADGE.STABLE],
+  },
+  argTypes: addArgTypes({
+    content: {
+      defaultValue: 'This is a button',
+      type: 'string',
+    },
+    type: {
+      options: ['primary', 'secondary'],
+      control: { type: 'radio' },
+    },
+    isDisabled: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+  }),
+}
 
-export const buttonDark = () => (
-  <Dark>
-    <Button />
-  </Dark>
-)
+export const ButtonOptions = addTheme((props) => <Button {...props}>{props.content}</Button>)
