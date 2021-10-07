31311562db3692d7dc9e4f0a79d66813c59f566d
rxluz <hi@rxluz.dev>
2021-10-07 11:38:09 +0100

    Updates

---
+++ storybook/precache-manifest.72fa1a61829d1cba0ae59b3fc9de5055.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee14088ec1aa60e72c6",
+    "url": "0.6b03dff6.iframe.bundle.js"
+  },
+  {
+    "revision": "924b2533654bc1bb41ce",
+    "url": "4.2ea5b505.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.2ea5b505.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "622e936d30e99170012e",
+    "url": "5.eb038fdc.iframe.bundle.js"
+  },
+  {
+    "revision": "4fd524f9ec8e68c1ef58",
+    "url": "6.e7edfaeb.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.e7edfaeb.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5e52a84dc43108755a",
+    "url": "7.995a709e.iframe.bundle.js"
+  },
+  {
+    "revision": "bfa3b740a02cbebfcaec0e31cc465291",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "6a6fcd6d75e0d1ca9c51",
+    "url": "main.67da9ee1.iframe.bundle.js"
+  },
+  {
+    "revision": "78a5a0270441d031ad21",
+    "url": "runtime~main.ef96f70c.iframe.bundle.js"
+  },
+  {
+    "revision": "6a6fcd6d75e0d1ca9c51",
+    "url": "static/css/main.4b434372.chunk.css"
+  },
+  {
+    "revision": "b2695cc66b3d4d85fd1924538870f2f4",
+    "url": "static/media/companyCardBackground.b2695cc6.png"
+  },
+  {
+    "revision": "90faf95329382801a0d4a30b72324ae0",
+    "url": "static/media/email.90faf953.svg"
+  },
+  {
+    "revision": "7c36c55ccf0eb763b589",
+    "url": "vendors~main.da7862d2.iframe.bundle.js"
+  },
+  {
+    "revision": "66cd5397389742deef1a030514cbb0c6",
+    "url": "vendors~main.da7862d2.iframe.bundle.js.LICENSE.txt"
+  }
+]);
