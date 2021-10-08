74c7a5aa82f069511b7e3a05797ed0f3ae960e2c
rxluz <hi@rxluz.dev>
2021-10-08 13:11:49 +0100

    feat(tests): introduce input tests

--- src/common/components/input/Input.spec.js
+++ src/common/components/input/Input.spec.js
@@ -1,9 +1,57 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import Input from '../Input'
+import Input from './Input'
 
-test('Renders input element properly', () => {
-  const { getByText } = render(<Input />)
-  const linkElement = getByText(/Hello/i)
+const defaultProps = {
+  title: 'input title',
+  id: 'input-1',
+  placeholder: 'input placeholder',
+  value: 'value',
+  type: 'text',
+  disabled: false,
+  showClearButton: false,
+  isValid: true,
+  onChange: () => {},
+}
+
+test('Renders input element title  properly', () => {
+  const { getByLabelText } = render(<Input {...defaultProps} />)
+  const linkElement = getByLabelText(/title/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+test('Renders input element placeholder properly', () => {
+  const { getByPlaceholderText } = render(<Input {...defaultProps} />)
+  const linkElement = getByPlaceholderText(/placeholder/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('Renders input element value properly', () => {
+  const { getByDisplayValue } = render(<Input {...defaultProps} />)
+  const linkElement = getByDisplayValue(/value/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('Renders input element is not disabled properly', () => {
+  const { getByDisplayValue } = render(<Input {...defaultProps} />)
+  const linkElement = getByDisplayValue(/value/i)
+  expect(linkElement).not.toBeDisabled()
+})
+
+test('Renders input element is disabled properly', () => {
+  const { getByDisplayValue } = render(<Input {...defaultProps} disabled={true} />)
+  const linkElement = getByDisplayValue(/value/i)
+  expect(linkElement).toBeDisabled()
+})
+
+test('Renders input element warning is rendered properly', () => {
+  const { getByRole } = render(
+    <Input
+      {...defaultProps}
+      isValid={false}
+      validation={{ test: () => false, message: () => 'validation message' }}
+    />,
+  )
+  const linkElement = getByRole('alert')
+  expect(linkElement.classList.contains('input__warning')).toBe(true)
 })
