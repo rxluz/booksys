1c2725dd1de52bde05c3e7b7e52d854cc2a41451
rxluz <hi@rxluz.dev>
2021-10-08 08:31:11 +0100

    feat(accessibility): introduce office card improvements

--- client/src/common/components/officeCard/OfficeCard.js
+++ client/src/common/components/officeCard/OfficeCard.js
@@ -6,23 +6,34 @@ import Button from 'common/components/button'
 
 import './OfficeCard.scss'
 
-const OfficeCard = ({ time, seat, onBook, translate }) => (
-  <div className="office-card animate__animated animate__fadeIn">
-    <div className="office-card__content">
-      <div className="office-card__time">
-        {moment.unix(time).format(generalConstants.MOMENT_TIME)}
-      </div>
-      <div className="office-card__seats">
-        <div className="office-card__seats--icon">
-          <BiChair />
-        </div>
-        <div className="office-card__seats--text">{seat}</div>
-      </div>
-    </div>
-    <div className="office-card__footer">
-      <Button onClick={() => onBook({ seat, time })}>{translate('Book')}</Button>
-    </div>
-  </div>
-)
+const OfficeCard = ({ time, seat, onBook, translate }) => {
+  const timeFormatted = moment.unix(time).format(generalConstants.MOMENT_TIME)
 
+  return (
+    <div className="office-card animate__animated animate__fadeIn" role="listitem">
+      <div className="office-card__content">
+        <div
+          className="office-card__time"
+          aria-label={translate('Time: {time}', { time: timeFormatted })}
+        >
+          {timeFormatted}
+        </div>
+        <div className="office-card__seats">
+          <div className="office-card__seats--icon" role="presentation" aria-hidden="true">
+            <BiChair />
+          </div>
+          <div
+            className="office-card__seats--text"
+            aria-label={translate('Seats available: {seat}', { seat })}
+          >
+            {seat}
+          </div>
+        </div>
+      </div>
+      <div className="office-card__footer">
+        <Button onClick={() => onBook({ seat, time })}>{translate('Book')}</Button>
+      </div>
+    </div>
+  )
+}
 export default OfficeCard
