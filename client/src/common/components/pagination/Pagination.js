f4803a1f020f20bff24c38c34b7c9b29b4836c4b
rxluz <hi@rxluz.dev>
2021-10-08 08:31:19 +0100

    feat(accessibility): introduce pagination improvements

--- client/src/common/components/pagination/Pagination.js
+++ client/src/common/components/pagination/Pagination.js
@@ -2,8 +2,17 @@ import React from 'react'
 import PaginationLib from 'react-responsive-pagination'
 import './Pagination.scss'
 
-const Pagination = ({ current, total, onPageChange }) => (
-  <PaginationLib current={current} total={total} onPageChange={onPageChange} />
+const Pagination = ({ current, total, onPageChange, label, translate }) => (
+  <nav
+    role="slider"
+    aria-label={label}
+    aria-valuenow={current}
+    aria-valuemin="1"
+    aria-valuemax={total}
+    aria-valuetext={translate('Page {num}', { num: current })}
+  >
+    <PaginationLib current={current} total={total} onPageChange={onPageChange} />
+  </nav>
 )
 
 export default Pagination
