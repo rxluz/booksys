716653e7a239a24c8c45a188f5ae6b31b5566910
rxluz <hi@rxluz.dev>
2021-10-08 16:45:05 +0100

    feat(test): introduce company card component tests

--- src/common/components/companyCard/CompanyCard.spec.js
+++ src/common/components/companyCard/CompanyCard.spec.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import CompanyCard from './CompanyCard'
+import * as testUtils from 'common/utils/test.utils'
 
-const defaultProps = {
-  translate: (value) => value,
+const defaultProps = testUtils.createDefaultProps({
   event: {
     name: 'event name',
     eventStartTime: 324324324324,
     address: 'Grand canal dock',
   },
-}
+})
 
 test('Renders company card element event name properly', () => {
   const { getByText } = render(<CompanyCard {...defaultProps}> hello </CompanyCard>)
