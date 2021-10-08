d0850fb47e03f7ba464c7d2309aaf1a5745d3183
rxluz <hi@rxluz.dev>
2021-10-08 13:11:14 +0100

    feat(tests): introduce button tests

--- src/common/components/button/Button.spec.js
+++ src/common/components/button/Button.spec.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import Button from '../Button'
+import Button from './Button'
 
-test('Renders button element properly', () => {
-  const { getByText } = render(<Input />)
-  const linkElement = getByText(/Hello/i)
+const defaultProps = {
+  styleType: 'primary',
+  onClick: () => {},
+  isDisabled: false,
+}
+
+test('Renders button content properly', () => {
+  const { getByText } = render(<Button {...defaultProps}>Hello mybutton</Button>)
+  const linkElement = getByText(/mybutton/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+test('Renders button style properly', () => {
+  const { getByText } = render(<Button {...defaultProps}>Hello mybutton</Button>)
+  const linkElement = getByText(/mybutton/i)
+  expect(linkElement.classList.contains('button--primary')).toBe(true)
+})
+
+test('Renders button element is disabled properly', () => {
+  const { getByText } = render(
+    <Button {...defaultProps} isDisabled={true}>
+      myButton
+    </Button>,
+  )
+  const linkElement = getByText('myButton')
+  expect(linkElement).toBeDisabled()
 })
