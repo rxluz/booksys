754c6990713f904af5081d7bdf6351c0343cbb89
rxluz <hi@rxluz.dev>
2021-10-08 18:03:19 +0100

    feat(stateless-component)

--- src/modules/company/Company.page.js
+++ src/modules/company/Company.page.js
@@ -3,6 +3,7 @@ import CompanyCard from 'common/components/companyCard'
 import * as PropTypes from 'prop-types'
 import * as companyConstants from './Company.constants'
 import Loading from 'common/components/loading'
+import NotFound from 'modules/main/NotFound'
 
 const CompanyPage = ({ state, children, translate, data }) => {
   const INTERNAL_PAGES = () => (
@@ -14,7 +15,7 @@ const CompanyPage = ({ state, children, translate, data }) => {
   const PAGES_BY_STATE = {
     loading: () => <Loading translate={translate} />,
     loaded: INTERNAL_PAGES,
-    failed: () => <>{translate('Invalid page')}</>,
+    failed: () => <NotFound translate={translate} />,
     expired: INTERNAL_PAGES,
   }
 
